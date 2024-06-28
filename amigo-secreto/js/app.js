6d9559d14b225a32c22ef28bd370cc9c839dacb5
unamos-cani <nicaunamos@gmail.com>
2024-06-28 15:04:43 -0300

    Add files via upload

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,88 @@
+let amigos = [];
+// botao adicionar
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+    
+    amigos.push(amigo.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+
+    amigo.value = '';
+
+    atualizarLista();
+    atualizarSorteio();
+}
+
+// botao sortear 
+function sortear(){
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    
+    for(let i = 0;i < amigos.length; i++){
+
+        if(i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' ---> ' + amigos[0] + '<br>';
+        }else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' ---> ' + amigos[i + 1] + '<br>';
+        }
+    }
+}
+
+
+function excluirAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+    atualizarSorteio();
+}
+
+// embaralhar os nomes
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+
+    for (let i = 0; i < amigos.length; i++) {
+        // Cria um elemento de parágrafo para cada amigo
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = amigos[i];
+       
+        // Adiciona um evento de clique para excluir o amigo
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+
+        // Adiciona o parágrafo à lista
+        lista.appendChild(paragrafo);
+    }
+}
+
+// botao reiniciar
+function reiniciar() {
+    amigos = [];   
+    document.getElementById('lista-amigos').innerHTML = '';
+    sorteio = document.getElementById('lista-sorteio').innerHTML =  ''; 
+}
+
